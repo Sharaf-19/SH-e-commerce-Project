54a3c18f0850b13303edddfb1c40c022d8f7c149
Sharaf19 <sharafmo1994@gmail.com>
2023-09-10 00:25:54 +0530

    editing and designing the pages & login page

--- javascript-amazon-project-main/scripts/s.js
+++ javascript-amazon-project-main/scripts/s.js
@@ -16,6 +16,13 @@ products.forEach((product) => {
         ${product.name}
       </div>
 
+      
+
+      <div class="product-price">
+        $${formatCurrency(product.priceCents)}
+      </div>
+
+
       <div class="product-rating-container">
         <img class="product-rating-stars"
           src="images/ratings/rating-${product.rating.stars * 10}.png">
@@ -24,24 +31,7 @@ products.forEach((product) => {
         </div>
       </div>
 
-      <div class="product-price">
-        $${formatCurrency(product.priceCents)}
-      </div>
-
-      <div class="product-quantity-container">
-        <select>
-          <option selected value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
-      </div>
+      
 
       <div class="product-spacer"></div>
 
